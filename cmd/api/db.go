bdc6ffec1bf2d38dda31738502f391e625438259
Manav Mahesh Sanger <karnophile@gmail.com>
2026-01-12 14:56:04 +0530

    feat: integrate postgres and use env vars for config

---
+++ cmd/api/db.go
@@ -0,0 +1,27 @@
+package main 
+
+import (
+	"database/sql"
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+// openDB opens a connection to the database
+// param dsn : Data Source Name (The address string of the DB) 
+// it returns a generic *sql.DB pool (The handle to use the DB) that is safe for concurrent use
+func openDB(dsn string) (*sql.DB, error) {
+	// open the connection 
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// testing the connection
+	// sql.open() doesnt connect immediately, it is lazy
+	// db.Ping() forces a connection to ensure DB is actually alive
+	// If the DB is down or password is wrong, this will fail.
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
